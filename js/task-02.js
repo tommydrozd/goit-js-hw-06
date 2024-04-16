97336ad125aa1a51759c41f90eedeeed0457f350
tommydrozd <tomekdro@gmail.com>
2024-04-16 21:01:15 +0200

    02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.createElement('ul');
+
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  list.appendChild(listItem);
+});
+
+document.body.appendChild(list);
